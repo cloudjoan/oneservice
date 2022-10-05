47c89fcdef9b4a2bc6ddbf602c971f57d9477217
leon.huang <leon.huang@etatung.com>
2022-10-05 15:35:32 +0800

    OneService init

---
+++ OneService/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OneService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
