661418cd8a6f1e4761d8ca16d7999c2254c8559f
leon.huang <leon.huang@etatung.com>
2022-12-02 10:55:11 +0800

    update

--- OneService/Models/ERP_PROXY_DBContext.cs
+++ OneService/Models/ERP_PROXY_DBContext.cs
@@ -23,7 +23,7 @@ namespace OneService.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=TSTI-SAP-PROXY;Database=ERP_PROXY_DB;User=sa;Password=Eip@dmin");
+                optionsBuilder.UseSqlServer("Server=172.31.7.40;Database=ERP_PROXY_DB;User=sa;Password=Eip@dmin");
             }
         }
 
--- OneService/Models/MCSWorkflowContext.cs
+++ OneService/Models/MCSWorkflowContext.cs
@@ -26,7 +26,7 @@ namespace OneService.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=TSTI-EIPDB;Database=MCSWorkflow;User=eip;Password=70771557");
+                optionsBuilder.UseSqlServer("Server=172.31.7.26;Database=MCSWorkflow;User=eip;Password=70771557");
             }
         }
 
