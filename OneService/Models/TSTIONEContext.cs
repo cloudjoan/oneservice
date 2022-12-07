4235e9f5f130daed34b7fa388458f649d62a276c
張豐穎 Elvis Chang <Elvis.Chang@etatung.com>
2022-12-07 11:38:52 +0800

    簽入

--- OneService/Models/TSTIONEContext.cs
+++ OneService/Models/TSTIONEContext.cs
@@ -33,7 +33,7 @@ namespace OneService.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=PSIP-QAS;Database=TSTI-ONE;User=psip;Password=einck!@!NNd");
+                optionsBuilder.UseSqlServer("Server=172.31.7.67;Database=TSTI-ONE;User=psip;Password=einck!@!NNd");
             }
         }
 
