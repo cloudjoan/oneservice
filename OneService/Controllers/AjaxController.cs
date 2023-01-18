333c37c5dc804622548cbcbd262098e4e7b71d8e
leon.huang <leon.huang@etatung.com>
2023-01-18 10:56:11 +0800

    Fix file upload

--- OneService/Controllers/AjaxController.cs
+++ OneService/Controllers/AjaxController.cs
@@ -20,47 +20,47 @@ namespace OneService.Controllers
 			return View();
 		}
 
-        [HttpPost]
-        public ActionResult AjaxFileUpload(IFormFile upload, bool needResize, int? width, int? height)
-        {
-            try
-            {
-                CkFileBean bean = new CkFileBean();
-                if (upload != null)
-                {
-                    string webRootPath = _HostEnvironment.WebRootPath + "/upload";
+        //[HttpPost]
+        //public ActionResult AjaxFileUpload(IFormFile upload, bool needResize, int? width, int? height)
+        //{
+        //    try
+        //    {
+        //        CkFileBean bean = new CkFileBean();
+        //        if (upload != null)
+        //        {
+        //            string webRootPath = _HostEnvironment.WebRootPath + "/upload";
 
-                    string fileId = Guid.NewGuid().ToString();
-                    string fileOrgName = upload.FileName;
-                    string fileName = fileId + Path.GetExtension(upload.FileName);
-                    string path = Path.Combine(webRootPath, fileName);
-                    bean.fileId = fileId;
-                    bean.fileName = fileName;
-                    bean.fileOrgName = fileOrgName;
-                    bean.url = "https://www.etatung.com/upload/" + fileName;
-                    bean.uploaded = 1;
-                    using (Stream fileStream = new FileStream(path, FileMode.Create))
-                    {
-                        upload.CopyToAsync(fileStream);
-                    }
-                    //縮圖
-                    //if (needResize)
-                    //{
-                    //    Image img = resizeImageFromFile(path, (int)width, (int)height, true, true);
-                    //    img.Save(path);
-                    //    GetPicThumbnail(path, path, 80);
-                    //}
+        //            string fileId = Guid.NewGuid().ToString();
+        //            string fileOrgName = upload.FileName;
+        //            string fileName = fileId + Path.GetExtension(upload.FileName);
+        //            string path = Path.Combine(webRootPath, fileName);
+        //            bean.fileId = fileId;
+        //            bean.fileName = fileName;
+        //            bean.fileOrgName = fileOrgName;
+        //            bean.url = "https://www.etatung.com/upload/" + fileName;
+        //            bean.uploaded = 1;
+        //            using (Stream fileStream = new FileStream(path, FileMode.Create))
+        //            {
+        //                upload.CopyToAsync(fileStream);
+        //            }
+        //            //縮圖
+        //            //if (needResize)
+        //            //{
+        //            //    Image img = resizeImageFromFile(path, (int)width, (int)height, true, true);
+        //            //    img.Save(path);
+        //            //    GetPicThumbnail(path, path, 80);
+        //            //}
 
-                }
-                ViewBag.Message = "File Uploaded Successfully!!";
-                return Json(bean);
-            }
-            catch
-            {
-                ViewBag.Message = "File upload failed!!";
-                return View();
-            }
-        }
+        //        }
+        //        ViewBag.Message = "File Uploaded Successfully!!";
+        //        return Json(bean);
+        //    }
+        //    catch
+        //    {
+        //        ViewBag.Message = "File upload failed!!";
+        //        return View();
+        //    }
+        //}
 
 
 
