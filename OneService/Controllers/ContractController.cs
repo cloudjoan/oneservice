f3bad22859d4949a6e625c08f50a241c09970ced
豐穎 張 <Elvis.Chang@etatung.com>
2023-05-23 13:53:31 +0800

    update

---
+++ OneService/Controllers/ContractController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OneService.Controllers
+{
+    public class ContractController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ContractMain()
+        {
+            return View();
+        }
+    }
+}
